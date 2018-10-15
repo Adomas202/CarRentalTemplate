4ac2ffdf9559339ce8a1845c23d58b5d148b61ef
Poleninas <adomas202@gmail.com>
2018-10-15 23:51:52 +0300

    New classes added structure

---
+++ src/KainosPolitikaPaprasta_DraudimasZalias.java
@@ -0,0 +1,11 @@
+public class KainosPolitikaPaprasta_DraudimasZalias extends NuomosUzsakymas {
+    @Override
+    public float PridetiDraudimoKaina(float kaina, int metai, int galingumas) {
+        return 0;
+    }
+
+    @Override
+    public float IkainuotiUzsakymoNuomosKaina(float kaina, int metai) {
+        return 0;
+    }
+}
---
+++ src/KainosPolitikaSudetinga_DraudimasRaudonas.java
@@ -0,0 +1,2 @@
+public class KainosPolitikaSudetinga_DraudimasRaudonas {
+}
